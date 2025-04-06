898bc31efc2a4d767a28f06041533213a3f14921
Horquidia <horquidia.a.lima@gmail.com>
2025-04-06 15:04:37 -0300

    Carregando os horários

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,17 @@
+import dayjs from "dayjs"
+import {openingHours} from "../../utils/opening.hours.js"
+
+export function hoursLoad({date}){
+    const opening = openingHours.map((hour) => {
+       //Recupera somente a hora
+       const [scheduleHour] = hour.split(":")
+
+       //Adciona a hora na data e verifica se esta no passado
+       const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+
+       return {
+        hour,
+        avaible: isHourPast,
+       }
+    })
+}
