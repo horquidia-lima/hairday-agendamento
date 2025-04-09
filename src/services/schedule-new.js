d0e33e9ea2c343c6c653f4d85fa11ca54cb74ed8
Horquidia <horquidia.a.lima@gmail.com>
2025-04-09 14:55:34 -0300

    Criando o serviço de agendamento

---
+++ src/services/schedule-new.js
@@ -0,0 +1,18 @@
+import { apiConfig } from "./api-config.js";
+
+export async function sheduleNew({id, name, when}){
+    try {
+        await fetch(`${apiConfig.baseUrl}/schedules`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({id, name, when}),
+        })
+
+        alert("Agendamento criado com sucesso")
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível criar o agendamento")
+    }
+}
